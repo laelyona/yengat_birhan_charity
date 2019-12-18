a61cc981c9b3a31350ac9fef462ebc585e4c0758
laelyona <dellyona@gmail.com>
2019-12-18 11:58:33 +0300

    UI added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  get 'home_page/Index'
+  #get 'home_page/Index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root to: "home_page#Index"
+  get 'home_page/about'
+  get 'home_page/contact'
 end
